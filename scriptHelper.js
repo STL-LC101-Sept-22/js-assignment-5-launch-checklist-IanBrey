62e75d8716b6b3455a654119db764fe7cf5ff49b
Ian Brey <ianbrey@Ians-iMac.local>
2022-12-06 20:10:58 -0800

    Fixed some superfluous issues with autograder

--- scriptHelper.js
+++ scriptHelper.js
@@ -35,31 +35,31 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     const launchStatus = document.getElementById("launchStatus");
 
 
-   pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch.`
-   copilotStatus.innerHTML = `Copilot ${copilot} is ready for launch.`
+   pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`
+   copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`
 
    // Fuel checks
    if (fuelLevel < 10000) {
     list.style.visiblity = "visible";
-    launchStatus.style.color = "red";
-    launchStatus.innerHTML = `Shuttle not ready for launch.`
-    fuelStatus.innerHTML = `Insufficient fuel level for journey.`
+    launchStatus.style.color = "rgb(199, 37, 78)";
+    launchStatus.innerHTML = `Shuttle Not Ready For Launch`
+    fuelStatus.innerHTML = `Fuel level too low for launch`
    } else {
-    fuelStatus.innerHTML = `Sufficient fuel level for journey`
+    fuelStatus.innerHTML = `Fuel level high enough for launch`
    }
 
    //Cargo checks
    if (cargoLevel > 10000) {
     list.style.visibility = "visible";
-    launchStatus.innerHTML = `Shuttle not ready for launch.`
-    launchStatus.style.color = "red";
-    cargoStatus.innerHTML = `Exceeding maximum mass capacity. Cannot takeoff.`
+    launchStatus.innerHTML = `Shuttle Not Ready For Launch`
+    launchStatus.style.color = "rgb(199, 37, 78)";
+    cargoStatus.innerHTML = `Cargo mass too heavy for launch`
    } else {
-    cargoStatus.innerHTML = `Cargo within acceptable capacity.`
+    cargoStatus.innerHTML = `Cargo mass low enough for launch`
    }
    if (fuelLevel >= 10000 && cargoLevel <= 10000) {
-    launchStatus.style.color = "green"
-    launchStatus.innerHTML = "Shuttle is ready for launch"
+    launchStatus.style.color = "rgb(65, 159, 106)"
+    launchStatus.innerHTML = "Shuttle is Ready for Launch"
    }
 }
 
